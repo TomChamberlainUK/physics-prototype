69f4f75ddeaf236fe65f0a0f6c3054b9047821ef
Tom Chamberlain <thomas.e.chamberlain@bbc.co.uk>
2025-07-03 20:33:08 +0100

    Added App unit tests for rendering a circle

--- tests/App.test.ts
+++ tests/App.test.ts
@@ -1,8 +1,24 @@
 import { render, screen } from '@testing-library/svelte';
-import { describe, it, expect, beforeEach } from 'vitest';
+import { beforeAll, beforeEach, describe, expect, it, vi } from 'vitest';
 import App from '#/App.svelte';
 
 describe('App', () => {
+  const mockRenderer = vi.hoisted(() => vi.fn());
+  const mockCtxArc = vi.hoisted(() => vi.fn());
+  const mockCtxFill = vi.hoisted(() => vi.fn());
+
+  beforeAll(() => {
+    vi.mock(import('#/Renderer'), () => {
+      const Renderer = mockRenderer;
+      Renderer.prototype.ctx = {
+        fillStyle: '#000',
+        arc: mockCtxArc,
+        fill: mockCtxFill,
+      };
+      return { default: Renderer };
+    });
+  });
+
   beforeEach(() => {
     render(App);
   });
@@ -10,5 +26,22 @@ describe('App', () => {
   it('Should render a canvas', () => {
     const canvas = screen.getByTestId('canvas');
     expect(canvas).toBeInTheDocument();
+  });
+
+  it('Should instantiate a Renderer', () => {
+    expect(mockRenderer).toHaveBeenCalled();
+  });
+
+  it('Should render a white circle on the canvas', () => {
+    const canvas = screen.getByTestId<HTMLCanvasElement>('canvas');
+    expect(mockRenderer.prototype.ctx.fillStyle).toBe('white');
+    expect(mockCtxArc).toHaveBeenCalledWith(
+      canvas.width / 2,
+      canvas.height / 2,
+      64,
+      0,
+      2 * Math.PI,
+    );
+    expect(mockCtxFill).toHaveBeenCalled();
   });
 });

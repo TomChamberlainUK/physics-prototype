74cf5340c882cc50c502056568ff4692b6b7b84d
Tom Chamberlain <thomas.e.chamberlain@bbc.co.uk>
2025-07-04 20:32:24 +0100

    Fixed incorrect type

--- tests/utils/expectCallOrder.ts
+++ tests/utils/expectCallOrder.ts
@@ -1,6 +1,7 @@
-import { expect, type Mock } from 'vitest';
+import { expect } from 'vitest';
+import type { MockInstance } from 'vitest';
 
-export default function expectCallOrder(mocks: Mock[]) {
+export default function expectCallOrder(mocks: MockInstance[]) {
   for (let i = 1; i < mocks.length; i++) {
     expect(mocks[i].mock.invocationCallOrder[0])
       .toBeGreaterThan(mocks[i - 1].mock.invocationCallOrder[0]);

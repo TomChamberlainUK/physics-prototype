e2fdbc42899ae7d5a27b6d0de70f6a9a97e95a49
Tom Chamberlain <thomas.e.chamberlain@bbc.co.uk>
2025-07-20 16:29:15 +0100

    Reduced repeated logic

--- tests/Entity.test.ts
+++ tests/Entity.test.ts
@@ -1,11 +1,20 @@
 import { Component } from '#/components';
 import Entity from '#/Entity';
-import { describe, expect, it } from 'vitest';
+import { beforeEach, describe, expect, it } from 'vitest';
 
 describe('Entity', () => {
+  let entity: Entity;
+  let component: Component;
+
+  const componentName = 'TestComponent';
+
+  beforeEach(() => {
+    entity = new Entity();
+    component = new Component(componentName);
+  });
+
   describe('constructor()', () => {
     it('Should instantiate', () => {
-      const entity = new Entity();
       expect(entity).toBeInstanceOf(Entity);
       expect(entity.id).toBeTypeOf('string');
       expect(entity.components).toBeInstanceOf(Map);
@@ -14,26 +23,20 @@ describe('Entity', () => {
 
   describe('addComponent()', () => {
     it('Should add a component to the entity', () => {
-      const entity = new Entity();
-      const component = new Component('TestComponent');
       entity.addComponent(component);
-      const addedComponent = entity.components.get('TestComponent');
+      const addedComponent = entity.components.get(componentName);
       expect(addedComponent).toBe(component);
     });
   });
 
   describe('getComponent()', () => {
     it('Should return the component if it exists', () => {
-      const entity = new Entity();
-      const component = new Component('TestComponent');
       entity.addComponent(component);
-      const retrievedComponent = entity.getComponent('TestComponent');
+      const retrievedComponent = entity.getComponent(componentName);
       expect(retrievedComponent).toBe(component);
     });
 
     it('Should throw an error if the component does not exist', () => {
-      const entity = new Entity();
-      const componentName = 'TestComponent';
       expect(() => entity.getComponent(componentName))
         .toThrowError(`Could not find component "${componentName}" on entity "${entity.id}"`);
     });
@@ -41,30 +44,23 @@ describe('Entity', () => {
 
   describe('hasComponent()', () => {
     it('Should return true if the component exists', () => {
-      const entity = new Entity();
-      const component = new Component('TestComponent', {});
       entity.addComponent(component);
-      expect(entity.hasComponent('TestComponent')).toBe(true);
+      expect(entity.hasComponent(componentName)).toBe(true);
     });
 
     it('Should return false if the component does not exist', () => {
-      const entity = new Entity();
-      expect(entity.hasComponent('TestComponent')).toBe(false);
+      expect(entity.hasComponent(componentName)).toBe(false);
     });
   });
 
   describe('removeComponent()', () => {
     it('Should remove a component from the entity', () => {
-      const entity = new Entity();
-      const component = new Component('TestComponent', {});
       entity.addComponent(component);
-      entity.removeComponent('TestComponent');
-      expect(entity.hasComponent('TestComponent')).toBe(false);
+      entity.removeComponent(componentName);
+      expect(entity.hasComponent(componentName)).toBe(false);
     });
 
     it('Should throw an error if trying to remove a non-existent component', () => {
-      const entity = new Entity();
-      const componentName = 'TestComponent';
       expect(() => entity.removeComponent(componentName))
         .toThrowError(`Could not remove component "${componentName}" from entity "${entity.id}" because it does not exist`);
     });

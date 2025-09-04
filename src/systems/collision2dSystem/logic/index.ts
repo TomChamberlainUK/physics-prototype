efc43ef53886ccc5697bf9dbe1ef01d930f05a9a
Tom Chamberlain <thomas.e.chamberlain@bbc.co.uk>
2025-09-04 19:07:29 +0100

    Remove deprecated legacy collision2dSystem

--- src/systems/collision2dSystem/logic/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as checkCollision } from './checkCollision.js';
-export { default as resolveCollision } from './resolveCollision.js';
--- src/systems/index.ts
+++ src/systems/index.ts
@@ -1,4 +1,3 @@
-export { default as collision2dSystem } from './collision2dSystem';
 export { default as collisionDetection2dSystem } from './collisionDetection2dSystem';
 export { default as collisionImpulseResolution2dSystem } from './collisionImpulseResolution2dSystem';
 export { default as collisionPositionCorrection2dSystem } from './collisionPositionCorrection2dSystem';

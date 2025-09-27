0af06ae58cc6c6292c7a2ed57292052ff6958fac
Tom Chamberlain <thomas.e.chamberlain@bbc.co.uk>
2025-09-27 15:01:31 +0100

    chore(monorepo): abstracted eslint rules to custom config package

--- apps/sandbox/eslint.config.js
+++ apps/sandbox/eslint.config.js
@@ -1,36 +1,13 @@
-import js from '@eslint/js';
-import stylistic from '@stylistic/eslint-plugin';
 import { defineConfig } from 'eslint/config';
-import svelte from 'eslint-plugin-svelte';
-import globals from 'globals';
-import tseslint from 'typescript-eslint';
+import { svelte } from 'eslint-config-custom';
 import svelteConfig from './svelte.config.js';
 
 export default defineConfig(
-  js.configs.recommended,
-  {
-    ignores: ['src/*'],
-    languageOptions: { globals: globals.node },
-  },
-  {
-    files: ['src/*'],
-    languageOptions: { globals: globals.browser },
-  },
-  stylistic.configs.customize({
-    indent: 2,
-    semi: true,
-    quotes: 'single',
-    jsx: false,
-  }),
-  tseslint.configs.recommended,
-  svelte.configs.recommended,
+  ...svelte,
   {
     files: ['**/*.svelte', '**/*.svelte.js', '**/*.svelte.ts'],
     languageOptions: {
       parserOptions: {
-        projectService: true,
-        extraFileExtensions: ['.svelte'],
-        parser: tseslint.parser,
         svelteConfig,
       },
     },
@@ -39,13 +16,12 @@ export default defineConfig(
     languageOptions: {
       parserOptions: {
         projectService: {
-          allowDefaultProject: ['*.js'],
+          allowDefaultProject: [
+            '*.js',
+          ],
         },
         tsconfigRootDir: import.meta.dirname,
       },
     },
-  },
-  {
-    ignores: ['dist'],
   },
 );
--- packages/engine/eslint.config.js
+++ packages/engine/eslint.config.js
@@ -1,31 +1,16 @@
-import js from '@eslint/js';
-import stylistic from '@stylistic/eslint-plugin';
 import { defineConfig } from 'eslint/config';
-import globals from 'globals';
-import tseslint from 'typescript-eslint';
+import { browser } from 'eslint-config-custom';
 
 export default defineConfig([
-  js.configs.recommended,
-  {
-    ignores: ['src/*'],
-    languageOptions: { globals: globals.node },
-  },
-  {
-    files: ['src/*'],
-    languageOptions: { globals: globals.browser },
-  },
-  stylistic.configs.customize({
-    indent: 2,
-    semi: true,
-    quotes: 'single',
-    jsx: false,
-  }),
-  tseslint.configs.recommended,
+  ...browser,
   {
     languageOptions: {
       parserOptions: {
         projectService: {
-          allowDefaultProject: ['*.js'],
+          allowDefaultProject: [
+            '*.js',
+            'vitest.config.ts',
+          ],
         },
         tsconfigRootDir: import.meta.dirname,
       },

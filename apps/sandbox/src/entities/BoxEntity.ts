da0a3d49d28ac5683b212e8ab12e251803fdcd20
Tom Chamberlain <thomas.e.chamberlain@bbc.co.uk>
2025-10-05 12:03:29 +0100

    feat(sandbox): add BoxEntity and unit tests

---
+++ apps/sandbox/src/entities/BoxEntity.ts
@@ -0,0 +1,45 @@
+import { Collider2dComponent, Entity, Geometry2dComponent, RigidBody2dComponent, Transform2dComponent, Vector2d } from 'engine';
+
+type Props = {
+  position?: Vector2d;
+  width?: number;
+  height?: number;
+  mass?: number;
+  color?: string;
+};
+
+export default class BoxEntity extends Entity {
+  constructor({
+    position = new Vector2d({ x: 0, y: 0 }),
+    width = 16,
+    height = 16,
+    mass = 16,
+    color = 'white',
+  }: Props = {}) {
+    super();
+    this.addComponents([
+      new Transform2dComponent({
+        position,
+      }),
+      new Geometry2dComponent({
+        color,
+        shape: {
+          type: 'box',
+          width,
+          height,
+        },
+      }),
+      new Collider2dComponent({
+        shape: {
+          type: 'box',
+          width,
+          height,
+        },
+      }),
+      new RigidBody2dComponent({
+        mass,
+        restitution: 0.8,
+      }),
+    ]);
+  }
+}

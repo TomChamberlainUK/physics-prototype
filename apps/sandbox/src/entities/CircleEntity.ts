af5e3908d4dff84c348aed53c07e5ead6461eee6
Tom Chamberlain <thomas.e.chamberlain@bbc.co.uk>
2025-10-05 12:03:50 +0100

    feat(sandbox): add CircleEntity and unit tests

---
+++ apps/sandbox/src/entities/CircleEntity.ts
@@ -0,0 +1,38 @@
+import { Collider2dComponent, Entity, Geometry2dComponent, RigidBody2dComponent, Transform2dComponent, Vector2d } from 'engine';
+
+type Props = {
+  position?: Vector2d;
+  radius?: number;
+  color?: string;
+};
+
+export default class CircleEntity extends Entity {
+  constructor({
+    position = new Vector2d({ x: 0, y: 0 }),
+    radius = 16,
+    color = 'white',
+  }: Props = {}) {
+    super();
+    this.addComponents([
+      new Transform2dComponent({
+        position,
+      }),
+      new Geometry2dComponent({
+        color,
+        shape: {
+          type: 'circle',
+          radius: radius,
+        },
+      }),
+      new RigidBody2dComponent({
+        mass: radius,
+      }),
+      new Collider2dComponent({
+        shape: {
+          type: 'circle',
+          radius: radius,
+        },
+      }),
+    ]);
+  }
+}

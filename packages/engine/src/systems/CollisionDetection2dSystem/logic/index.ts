7d3bfc185b47519dae3128f2d77cf5fd7f239020
Tom Chamberlain <thomas.e.chamberlain@bbc.co.uk>
2025-10-15 21:15:20 +0100

    feat(engine): add getBroadPhasePairs logic

--- packages/engine/src/systems/CollisionDetection2dSystem/logic/index.ts
+++ packages/engine/src/systems/CollisionDetection2dSystem/logic/index.ts
@@ -1,5 +1,6 @@
 export { default as getAABBOverlap } from './getAABBOverlap';
 export { default as getBoxBoxCollision } from './getBoxBoxCollision';
 export { default as getBoxCircleCollision } from './getBoxCircleCollision';
+export { default as getBroadPhasePairs } from './getBroadPhasePairs';
 export { default as getCircleCircleCollision } from './getCircleCircleCollision';
 export { default as getCollision } from './getCollision';

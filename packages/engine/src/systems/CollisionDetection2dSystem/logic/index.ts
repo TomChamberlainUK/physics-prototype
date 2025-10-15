a8b7fe02badca305ca0e6655f2522b6c59426482
Tom Chamberlain <thomas.e.chamberlain@bbc.co.uk>
2025-10-15 21:16:01 +0100

    feat(engine): add getNarrowPhasePairs logic

--- packages/engine/src/systems/CollisionDetection2dSystem/logic/index.ts
+++ packages/engine/src/systems/CollisionDetection2dSystem/logic/index.ts
@@ -4,3 +4,4 @@ export { default as getBoxCircleCollision } from './getBoxCircleCollision';
 export { default as getBroadPhasePairs } from './getBroadPhasePairs';
 export { default as getCircleCircleCollision } from './getCircleCircleCollision';
 export { default as getCollision } from './getCollision';
+export { default as getNarrowPhasePairs } from './getNarrowPhasePairs';

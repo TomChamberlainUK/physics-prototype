ece8bd58db6565a40f656ac3d57c98b87b7c4fa6
Tom Chamberlain <thomas.e.chamberlain@bbc.co.uk>
2025-10-16 20:33:06 +0100

    feat(engine): add alpha and renderer properties to Context type

--- packages/engine/src/types/Context.ts
+++ packages/engine/src/types/Context.ts
@@ -1,8 +1,11 @@
 import type { KeyboardInput } from '#/input';
+import type Renderer from '#/Renderer';
 import type { CollisionPair } from './CollisionPair';
 
 export type Context = {
+  alpha?: number;
   collisionPairs?: CollisionPair[];
   deltaTime?: number;
   input?: KeyboardInput;
+  renderer?: Renderer;
 };

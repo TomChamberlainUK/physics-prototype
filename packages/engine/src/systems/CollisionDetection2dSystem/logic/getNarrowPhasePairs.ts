8c2dc55778b69c2ec10c96d6cba915cf4daf8a30
Tom Chamberlain <thomas.e.chamberlain@bbc.co.uk>
2025-10-22 15:44:34 +0100

    fix(engine): correct type reference to NarrowPhaseCollisionPair

--- packages/engine/src/systems/CollisionDetection2dSystem/logic/getNarrowPhasePairs.ts
+++ packages/engine/src/systems/CollisionDetection2dSystem/logic/getNarrowPhasePairs.ts
@@ -1,9 +1,9 @@
 import type Entity from '#/Entity';
-import type { CollisionPair } from '#/index';
+import type { NarrowPhaseCollisionPair } from '#/types';
 import getCollision from './getCollision';
 
 export default function getNarrowPhasePairs(candidatePairs: [Entity, Entity][]) {
-  const collisionPairs: CollisionPair[] = [];
+  const collisionPairs: NarrowPhaseCollisionPair[] = [];
   for (const [entityA, entityB] of candidatePairs) {
     const { isColliding, normal, overlap } = getCollision(entityA, entityB);
     if (!isColliding) {

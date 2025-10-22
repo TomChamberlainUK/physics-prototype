4b036415ff8b587bf9bc4407bb838606bbd5c70e
Tom Chamberlain <thomas.e.chamberlain@bbc.co.uk>
2025-10-22 16:51:58 +0100

    feat(engine): add getNarrowPhaseCollisionPairsMap

--- packages/engine/src/systems/RenderDebug2dSystem/logic/index.ts
+++ packages/engine/src/systems/RenderDebug2dSystem/logic/index.ts
@@ -1,3 +1,4 @@
 export { default as getBroadPhaseCollisionPairsSet } from './getBroadPhaseCollisionPairsSet';
+export { default as getNarrowPhaseCollisionPairsMap } from './getNarrowPhaseCollisionPairsMap';
 export { default as renderAABB } from './renderAABB';
 export { default as renderPotentialCollisionLine } from './renderPotentialCollisionLine';

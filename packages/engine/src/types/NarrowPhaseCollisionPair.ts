4b961a159cfa89fb895330f6d60cbf888e4060aa
Tom Chamberlain <thomas.e.chamberlain@bbc.co.uk>
2025-10-25 19:19:43 +0100

    docs(engine): add TSDoc comments to NarrowPhaseCollisionPair type

--- packages/engine/src/types/NarrowPhaseCollisionPair.ts
+++ packages/engine/src/types/NarrowPhaseCollisionPair.ts
@@ -1,9 +1,16 @@
 import type Entity from '#/Entity';
 import type Vector2d from '#/maths/Vector2d';
 
+/**
+ * A pair of entities that are confirmed to be colliding, along with collision details.
+ */
 export type NarrowPhaseCollisionPair = {
+  /** The first entity in the collision pair. */
   entityA: Entity;
+  /** The second entity in the collision pair. */
   entityB: Entity;
+  /** The normal vector at the point of collision. */
   normal: Vector2d;
+  /** The amount of overlap between the two entities. */
   overlap: number;
 };

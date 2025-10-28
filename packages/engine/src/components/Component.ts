0d8fd508b0aac23bff6d84184810a4892e64f5d0
Tom Chamberlain <thomas.e.chamberlain@bbc.co.uk>
2025-10-28 20:46:29 +0000

    docs(engine): add TSDoc comments to Component class

--- packages/engine/src/components/Component.ts
+++ packages/engine/src/components/Component.ts
@@ -1,6 +1,14 @@
-export default class Component {
+/**
+ * Base class for all components.
+ */
+export default abstract class Component {
+  /** The name of the component. */
   name: string;
 
+  /**
+   * Creates an instance of the Component class.
+   * @param name - The name of the component.
+   */
   constructor(name: string) {
     this.name = name;
   }

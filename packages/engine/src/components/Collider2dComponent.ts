6bbaec20cb2ccb7fdc285eabcc01dd2f124415f9
Tom Chamberlain <thomas.e.chamberlain@bbc.co.uk>
2025-10-28 18:15:59 +0000

    docs(engine): add TSDoc comments to Collider2dComponent class

--- packages/engine/src/components/Collider2dComponent.ts
+++ packages/engine/src/components/Collider2dComponent.ts
@@ -1,15 +1,28 @@
 import type { AABB, Shape } from '#/types';
 import Component from './Component';
 
-type Props = {
+/**
+ * Parameters for constructing a Collider2dComponent.
+ */
+type ConstructorParams = {
+  /** The shape of the collider. */
   shape: Shape;
 };
 
+/**
+ * A component that defines a 2D collider for an entity.
+ */
 export default class Collider2dComponent extends Component {
+  /** The axis-aligned bounding box of the collider. */
   aabb: AABB | null;
+  /** The shape of the collider. */
   shape: Shape;
 
-  constructor({ shape }: Props) {
+  /**
+   * Creates an instance of the Collider2dComponent.
+   * @param shape - The shape of the collider.
+   */
+  constructor({ shape }: ConstructorParams) {
     super('Collider2d');
     this.aabb = null;
     this.shape = shape;

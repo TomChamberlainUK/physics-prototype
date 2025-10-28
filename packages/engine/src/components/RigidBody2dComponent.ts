1138175ac1bb3821124bea86035effbfce4e65d3
Tom Chamberlain <thomas.e.chamberlain@bbc.co.uk>
2025-10-28 20:53:35 +0000

    docs(engine): add TSDoc comments to RigidBody2dComponent class

--- packages/engine/src/components/RigidBody2dComponent.ts
+++ packages/engine/src/components/RigidBody2dComponent.ts
@@ -1,26 +1,57 @@
 import { Vector2d } from '#/maths';
 import Component from './Component';
 
-type ConstructorProps = {
+/**
+ * Parameters for constructing a RigidBody2dComponent.
+ */
+type ConstructorParams = {
+  /** The velocity of the rigid body. */
   velocity?: Vector2d;
+  /** The acceleration of the rigid body. */
   acceleration?: Vector2d;
+  /** The force applied to the rigid body. */
   force?: Vector2d;
+  /** The impulse applied to the rigid body. */
   impulse?: Vector2d;
+  /** The mass of the rigid body. */
   mass?: number;
+  /** The restitution coefficient of the rigid body. */
   restitution?: number;
+  /** The friction coefficient of the rigid body. */
   friction?: number;
 };
 
+/**
+ * A component that defines a 2D rigid body for an entity.
+ */
 export default class RigidBody2dComponent extends Component {
+  /** The velocity of the rigid body. */
   velocity: Vector2d;
+  /** The acceleration of the rigid body. */
   acceleration: Vector2d;
+  /** The force applied to the rigid body. */
   force: Vector2d;
+  /** The impulse applied to the rigid body. */
   impulse: Vector2d;
+  /** The mass of the rigid body. */
   mass: number;
+  /** The inverse mass of the rigid body. */
   inverseMass: number;
+  /** The restitution coefficient of the rigid body. */
   restitution: number;
+  /** The friction coefficient of the rigid body. */
   friction: number;
 
+  /**
+   * Creates an instance of the RigidBody2dComponent.
+   * @param velocity - The velocity of the rigid body.
+   * @param acceleration - The acceleration of the rigid body.
+   * @param force - The force applied to the rigid body.
+   * @param impulse - The impulse applied to the rigid body.
+   * @param mass - The mass of the rigid body.
+   * @param restitution - The restitution coefficient of the rigid body.
+   * @param friction - The friction coefficient of the rigid body.
+   */
   constructor({
     velocity = new Vector2d(),
     acceleration = new Vector2d(),
@@ -29,7 +60,7 @@ export default class RigidBody2dComponent extends Component {
     mass = 1,
     restitution = 0.8,
     friction = 0.3,
-  }: ConstructorProps = {}) {
+  }: ConstructorParams = {}) {
     super('RigidBody2d');
     this.velocity = velocity;
     this.acceleration = acceleration;

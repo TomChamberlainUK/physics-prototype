5d36812d56fc6cc7266c2865719d2b23f2e6ce0b
Tom Chamberlain <thomas.e.chamberlain@bbc.co.uk>
2025-10-28 20:50:35 +0000

    docs(engine): add TSDoc comments to InputImpulseComponent class

--- packages/engine/src/components/InputImpulseComponent.ts
+++ packages/engine/src/components/InputImpulseComponent.ts
@@ -1,6 +1,12 @@
 import Component from './Component';
 
+/**
+ * A component that flags an entity to receive input impulses.
+ */
 export default class InputImpulseComponent extends Component {
+  /**
+   * Creates an instance of the InputImpulseComponent.
+   */
   constructor() {
     super('InputImpulse');
   }

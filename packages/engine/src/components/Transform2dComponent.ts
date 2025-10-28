59d641a76f7f54aa0bfac1ae83a869408136e530
Tom Chamberlain <thomas.e.chamberlain@bbc.co.uk>
2025-10-28 20:58:38 +0000

    docs(engine): add TSDoc comments to Transform2dComponent class

--- packages/engine/src/components/Transform2dComponent.ts
+++ packages/engine/src/components/Transform2dComponent.ts
@@ -1,23 +1,42 @@
 import { Vector2d } from '#/maths';
 import Component from './Component';
 
-type ConstructorProps = {
+/**
+ * Parameters for constructing a Transform2dComponent.
+ */
+type ConstructorParams = {
+  /** The position of the transform. */
   position?: Vector2d;
+  /** The rotation of the transform. */
   rotation?: number;
+  /** The scale of the transform. */
   scale?: Vector2d;
 };
 
+/**
+ * A component that defines a 2D transform for an entity.
+ */
 export default class Transform2dComponent extends Component {
+  /** The position of the transform. */
   position: Vector2d;
+  /** The previous position of the transform. */
   previousPosition: Vector2d;
+  /** The rotation of the transform. */
   rotation: number;
+  /** The scale of the transform. */
   scale: Vector2d;
 
+  /**
+   * Creates an instance of the Transform2dComponent.
+   * @param position - The position of the transform.
+   * @param rotation - The rotation of the transform.
+   * @param scale - The scale of the transform.
+   */
   constructor({
     position = new Vector2d(),
     rotation = 0,
     scale = new Vector2d(1, 1),
-  }: ConstructorProps = {}) {
+  }: ConstructorParams = {}) {
     super('Transform2d');
     this.position = position;
     this.previousPosition = new Vector2d(position);

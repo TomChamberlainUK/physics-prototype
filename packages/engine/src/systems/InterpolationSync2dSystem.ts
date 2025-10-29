35dcd0ae665593438748df216d353c7dd1408076
Tom Chamberlain <thomas.e.chamberlain@bbc.co.uk>
2025-10-29 17:14:16 +0000

    docs(engine): add TSDoc comments to InterpolationSync2dSystem class

--- packages/engine/src/systems/InterpolationSync2dSystem.ts
+++ packages/engine/src/systems/InterpolationSync2dSystem.ts
@@ -3,10 +3,17 @@ import type Entity from '#/Entity';
 import Vector2d from '#/maths/Vector2d';
 import System from './System';
 
+/**
+ * A system that synchronizes previous positions for 2D transforms to enable interpolation.
+ */
 export default class InterpolationSync2dSystem extends System {
   name = 'InterpolationSync2dSystem';
   type = 'sync';
 
+  /**
+   * Updates previous positions of 2D transforms to match current positions.
+   * @param entities - The entities in the system.
+   */
   update(entities: Entity[]) {
     for (const entity of entities) {
       if (!entity.hasComponents(['Transform2d'])) {

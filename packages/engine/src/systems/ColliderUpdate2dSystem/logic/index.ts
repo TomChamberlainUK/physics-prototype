5bd5f6adfa1f59e9dad2619255591971d7b58994
Tom Chamberlain <thomas.e.chamberlain@bbc.co.uk>
2025-11-17 18:09:50 +0000

    feat(engine): add getBoxVertices function

--- packages/engine/src/systems/ColliderUpdate2dSystem/logic/index.ts
+++ packages/engine/src/systems/ColliderUpdate2dSystem/logic/index.ts
@@ -1,3 +1,4 @@
 export { default as getAABB } from './getAABB';
 export { default as getBoxAABB } from './getBoxAABB';
+export { default as getBoxVertices } from './getBoxVertices';
 export { default as getCircleAABB } from './getCircleAABB';

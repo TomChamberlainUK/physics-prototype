98dc969f10c9dae66ab38526af8b7b94633eb2b5
Tom Chamberlain <thomas.e.chamberlain@bbc.co.uk>
2025-11-18 18:08:25 +0000

    feat(engine): add getBoxVertices logic for Collider2dComponent

---
+++ packages/engine/tests/components/Collider2dComponent/logic/getBoxVertices.test.ts
@@ -0,0 +1,19 @@
+import { describe, expect, it } from 'vitest';
+import { getBoxVertices } from '#/components/Collider2dComponent/logic';
+
+describe('getBoxVertices', () => {
+  it('Should return vertices for a box', () => {
+    const width = 4;
+    const height = 2;
+    const halfWidth = width / 2;
+    const halfHeight = height / 2;
+    const expectedVertices = [
+      { x: -halfWidth, y: -halfHeight },
+      { x: halfWidth, y: -halfHeight },
+      { x: halfWidth, y: halfHeight },
+      { x: -halfWidth, y: halfHeight },
+    ];
+    const vertices = getBoxVertices({ width, height });
+    expect(vertices).toEqual(expectedVertices);
+  });
+});

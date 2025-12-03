2c97eb570dd69f13dab967d074a680ac66f35bd2
Tom Chamberlain <thomas.e.chamberlain@bbc.co.uk>
2025-12-03 18:56:35 +0000

    feat(engine): add renderContactPoints RenderDebug2dSystem logic

--- packages/engine/src/systems/RenderDebug2dSystem/logic/index.ts
+++ packages/engine/src/systems/RenderDebug2dSystem/logic/index.ts
@@ -2,4 +2,5 @@ export { default as getBroadPhaseCollisionPairsSet } from './getBroadPhaseCollis
 export { default as getNarrowPhaseCollisionPairsMap } from './getNarrowPhaseCollisionPairsMap';
 export { default as renderAABB } from './renderAABB';
 export { default as renderCollider } from './renderCollider';
+export { default as renderContactPoints } from './renderContactPoints';
 export { default as renderPotentialCollisionLine } from './renderPotentialCollisionLine';

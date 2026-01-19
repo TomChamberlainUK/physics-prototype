49f9fbb731e9b39910608ef0ef3c737a95d818d0
Tom Chamberlain <thomas.e.chamberlain@bbc.co.uk>
2026-01-19 18:46:00 +0000

    docs(engine): add link to Shape type for getVertices parameters

--- packages/engine/src/components/Collider2dComponent/logic/getVertices.ts
+++ packages/engine/src/components/Collider2dComponent/logic/getVertices.ts
@@ -3,7 +3,7 @@ import getBoxVertices from './getBoxVertices';
 
 /**
  * Returns the vertices of a given shape.
- * @param shape - The shape for which to get the vertices.
+ * @param shape - The shape for which to get the vertices, see {@link Shape}.
  * @returns An array of vertices or null if the shape has no vertices.
  */
 export default function getVertices(shape: Shape) {

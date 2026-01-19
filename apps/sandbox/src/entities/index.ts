8eb8624445c8eac908667da2ee68ecee3232be9a
Tom Chamberlain <thomas.e.chamberlain@bbc.co.uk>
2026-01-19 18:50:01 +0000

    chore(sandbox): remove unused BoxEntity and CircleEntity classes

--- apps/sandbox/src/entities/index.ts
+++ apps/sandbox/src/entities/index.ts
@@ -1,4 +1,2 @@
-export { default as BoxEntity } from './BoxEntity';
-export { default as CircleEntity } from './CircleEntity';
 export { default as PhysicsEntity } from './PhysicsEntity';
 export { default as PlayerEntity } from './PlayerEntity';

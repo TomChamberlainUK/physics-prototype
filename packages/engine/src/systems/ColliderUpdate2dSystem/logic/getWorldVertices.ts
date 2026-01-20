07c45f8d5b7c5bd7785f4b5784b3f7eced169317
Tom Chamberlain <thomas.e.chamberlain@bbc.co.uk>
2026-01-20 18:27:08 +0000

    docs(engine): add getWorldVertices doc comments to describe function

--- packages/engine/src/systems/ColliderUpdate2dSystem/logic/getWorldVertices.ts
+++ packages/engine/src/systems/ColliderUpdate2dSystem/logic/getWorldVertices.ts
@@ -2,6 +2,11 @@ import type { Collider2dComponent, Transform2dComponent } from '#/components';
 import type Entity from '#/Entity';
 import { Matrix2d, Vector2d } from '#/maths';
 
+/**
+ * Retrieves the world vertices of a 2D collider attached to a given entity.
+ * @param entity - The entity containing the Collider2d and Transform2d components.
+ * @returns An array of world vertices or null if components are missing.
+ */
 export default function getWorldVertices(entity: Entity) {
   if (!entity.hasComponents(['Collider2d', 'Transform2d'])) {
     return null;

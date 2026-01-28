93db9a5253860cc7b6dc936171b7b3b826104b9f
Tom Chamberlain <thomas.e.chamberlain@bbc.co.uk>
2026-01-28 20:24:59 +0000

    style(engine): add whitespace to findBroadPhasePairs for better readability

--- packages/engine/src/systems/CollisionDetection2dSystem/logic/findBroadPhasePairs.ts
+++ packages/engine/src/systems/CollisionDetection2dSystem/logic/findBroadPhasePairs.ts
@@ -9,28 +9,37 @@ import areAABBsOverlapping from './areAABBsOverlapping';
  */
 export default function findBroadPhasePairs(entities: Entity[]) {
   const candidatePairs: [Entity, Entity][] = [];
+
   for (let i = 0; i < entities.length; i++) {
     const entityA = entities[i];
-    if (!entityA?.hasComponent('Collider2d')) {
+
+    if (!entityA || !entityA.hasComponent('Collider2d')) {
       continue;
     }
+
     for (let j = i + 1; j < entities.length; j++) {
       const entityB = entities[j];
+
       if (!entityB || !entityB.hasComponent('Collider2d')) {
         continue;
       }
+
       const colliderA = entityA.getComponent<Collider2dComponent>('Collider2d');
       const colliderB = entityB.getComponent<Collider2dComponent>('Collider2d');
+
       if (!colliderA.aabb || !colliderB.aabb) {
         candidatePairs.push([entityA, entityB]);
         continue;
       }
+
       const aabbOverlap = areAABBsOverlapping(colliderA.aabb, colliderB.aabb);
       if (!aabbOverlap) {
         continue;
       }
+
       candidatePairs.push([entityA, entityB]);
     }
   }
+
   return candidatePairs;
 }

a64116d71ddeaaf041a0e269ba6980e77802f644
Tom Chamberlain <thomas.e.chamberlain@bbc.co.uk>
2026-02-02 19:18:05 +0000

    feat(engine): add Gravity2dSystem

--- packages/engine/src/systems/index.ts
+++ packages/engine/src/systems/index.ts
@@ -2,6 +2,7 @@ export { default as ColliderUpdate2dSystem } from './ColliderUpdate2dSystem';
 export { default as CollisionDetection2dSystem } from './CollisionDetection2dSystem';
 export { default as CollisionImpulseResolution2dSystem } from './CollisionImpulseResolution2dSystem';
 export { default as CollisionPositionCorrection2dSystem } from './CollisionPositionCorrection2dSystem';
+export { default as Gravity2dSystem } from './Gravity2dSystem';
 export { default as InputImpulseSystem } from './InputImpulseSystem';
 export { default as InterpolationSync2dSystem } from './InterpolationSync2dSystem';
 export { default as Kinetic2dSystem } from './Kinetic2dSystem';

e5d456f908b7fee6148c3c5eb07bfbdf0dbfa412
Tom Chamberlain <thomas.e.chamberlain@bbc.co.uk>
2026-02-11 19:59:12 +0000

    test(engine): update expectCallOrder to verify call order of repeated and interleaved mocks

--- packages/engine/tests/utils/expectCallOrder.test.ts
+++ packages/engine/tests/utils/expectCallOrder.test.ts
@@ -1,31 +1,96 @@
-import { describe, expect, it, vi } from 'vitest';
+import { afterAll, afterEach, describe, expect, it, vi } from 'vitest';
 import expectCallOrder from './expectCallOrder';
 
 describe('expectCallOrder', () => {
-  it('Should verify the call order of mocked functions', () => {
-    const mock1 = vi.fn();
-    const mock2 = vi.fn();
-    const mock3 = vi.fn();
+  const mock1 = vi.fn();
+  const mock2 = vi.fn();
+  const mock3 = vi.fn();
 
+  afterEach(() => {
+    mock1.mockClear();
+    mock2.mockClear();
+    mock3.mockClear();
+  });
+
+  afterAll(() => {
+    mock1.mockReset();
+    mock2.mockReset();
+    mock3.mockReset();
+  });
+
+  it('Should verify the call order of mocked functions', () => {
     mock1();
     mock2();
     mock3();
 
-    expectCallOrder([
-      mock1,
-      mock2,
-      mock3,
-    ]);
+    expect(() => {
+      expectCallOrder([
+        mock1,
+        mock2,
+        mock3,
+      ]);
+    }).not.toThrowError();
+  });
 
-    if (
-      !mock1.mock.invocationCallOrder[0]
-      || !mock2.mock.invocationCallOrder[0]
-      || !mock3.mock.invocationCallOrder[0]
-    ) {
-      throw new Error('Mock functions were not called');
-    }
+  it('Should verify the call order of mocked functions when called multiple times', () => {
+    mock1();
+    mock1();
+    mock2();
+    mock2();
+    mock3();
+    mock3();
 
-    expect(mock1.mock.invocationCallOrder[0]).toBeLessThan(mock2.mock.invocationCallOrder[0]);
-    expect(mock2.mock.invocationCallOrder[0]).toBeLessThan(mock3.mock.invocationCallOrder[0]);
+    expect(() => {
+      expectCallOrder([
+        mock1,
+        mock2,
+        mock3,
+      ]);
+    }).not.toThrowError();
+  });
+
+  it('Should verify the call order of mocked functions when called multiple times in an interleaved manner', () => {
+    mock1();
+    mock2();
+    mock1();
+    mock3();
+    mock2();
+    mock3();
+
+    expect(() => {
+      expectCallOrder([
+        mock1,
+        mock2,
+        mock1,
+        mock3,
+        mock2,
+        mock3,
+      ]);
+    }).not.toThrowError();
+  });
+
+  it('Should throw an error if a mock is called fewer times than expected', () => {
+    mock1();
+    mock2();
+
+    expect(() => {
+      expectCallOrder([
+        mock1,
+        mock2,
+        mock3,
+      ]);
+    }).toThrowError();
+  });
+
+  it('Should throw an error if a mock is called out of order', () => {
+    mock2();
+    mock1();
+
+    expect(() => {
+      expectCallOrder([
+        mock1,
+        mock2,
+      ]);
+    }).toThrowError();
   });
 });
